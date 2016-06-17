9bdac58754bcb56d2118755fb36720adcd58b252
Ravi Kiran <raak@hackerstrip.com>
2016-06-17 10:05:27 +0530

    v2

--- lcd_driver.py
+++ lcd_driver.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 #
 # GrovePi Example for using the Grove - LCD RGB Backlight (http://www.seeedstudio.com/wiki/Grove_-_LCD_RGB_Backlight)
-#
-# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
 # -- Ravi Kiran. K added support for Jetson TK1 and TX1
 # sudo pip install netifaces
 
@@ -80,13 +78,3 @@ def setText_norefresh(text):
                 continue
         count += 1
         bus.write_byte_data(DISPLAY_TEXT_ADDR, 0x40, ord(c))
-
-# example code
-if __name__ == "__main__":
-    setText("Hello world\nThis is an LCD test")
-    setRGB(0, 128, 64)
-    for c in range(0, 255):
-        setRGB(c, 255 - c, 0)
-        time.sleep(0.01)
-    setRGB(0, 255, 0)
-    setText("Bye bye, this should wrap onto next line")
